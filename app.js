b64edb2b8132f700461f0517df782f42b5e0f8da
James Charlesworth <james.charlesworth@gmail.com>
2012-08-05 14:28:36 -0400

    added www redirect

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+app.get('*',function(req, res, next) {
+  if (req.headers.host.match(/^www/) !== null ) {
+    res.redirect('http://' + req.headers.host.replace(/^www\./, '') + req.url);
+  } else {
+    next();
+  }
+});
+
 app.get('/', routes.index);
 app.get('/about', routes.about );
 app.get('/projects*', routes.projects );
